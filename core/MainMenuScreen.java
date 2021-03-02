88c2f0f39d330e5fd27a82348ea6fb389c4fc243
Swimmov <dimtest191@gmail.com>
2021-03-02 11:52:48 -0600

    welcome screen bug fixed  on core/MainMenuScreen.java

--- core/MainMenuScreen.java
+++ core/MainMenuScreen.java
@@ -2,6 +2,7 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 
 public class MainMenuScreen implements Screen {
@@ -17,12 +18,15 @@ public class MainMenuScreen implements Screen {
 
 
     @Override
-    public void show() {
-
-    }
+    public void show() {    }
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClearColor(0, 1f, .5f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+
         game.batch.setProjectionMatrix(camera.combined);
         game.batch.begin();
 game.font.draw(game.batch, "Don't let it go!", 100, 150);
