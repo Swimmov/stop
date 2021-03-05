cdf8b9b2518d6eb6f59921a9a24228395d155a79
Swimmov <dimtest191@gmail.com>
2021-03-04 21:19:32 -0600

    all new .java files for realese 3 v2.1

--- desktop/DesktopLauncher.java
+++ desktop/DesktopLauncher.java
@@ -1,13 +1,13 @@
-package com.mygdx.game.desktop;
+package com.myfkd.stopit.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.mygdx.game.Stopee;
+import com.myfkd.stopit.Stopee;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "StopTarakan";
+		config.title = "StopIt";
 		config.width = 800;
 		config.height = 400;
 		new LwjglApplication(new Stopee(), config);
